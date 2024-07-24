d1fcad00a2f21ebd647906d5ca80862baa2f1b1c
acapzine <119457323+acapzine@users.noreply.github.com>
2024-07-24 09:59:17 -0400

    add the code and dataset + the package(-lock).json

---
+++ index.js
@@ -0,0 +1,55 @@
+import { readFile } from "node:fs/promises";
+import readline from "node:readline";
+import pkg from "natural";
+import chalk from "chalk";
+const { JaroWinklerDistance } = pkg;
+
+const loadData = async (filePath) => {
+  try {
+    const data = await readFile(filePath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    throw new Error(`Error reading file: ${error}`);
+  }
+};
+
+const computeSimilarity = (input, data) => {
+  const crimeWeight = 0.7;
+  const detailsWeight = 0.3;
+
+  return data.map((entry) => {
+    const crimeSimilarity = JaroWinklerDistance(input.crime, entry.crime);
+    let detailsSimilarity = JaroWinklerDistance(input.details, entry.details);
+
+    const totalSimilarity =
+      crimeSimilarity * crimeWeight + detailsSimilarity * detailsWeight;
+    return { ...entry, similarity: totalSimilarity };
+  });
+};
+
+const findMostSimilarCase = (similarities) => {
+  return similarities.reduce((prev, curr) =>
+    curr.similarity > prev.similarity ? curr : prev,
+  );
+};
+
+(async () => {
+  const data = await loadData("./data.json");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("Enter the crime: ", (crime) => {
+    rl.question("Enter the details of the crime: ", async (details) => {
+      const inputCase = { crime, details };
+      const similarities = computeSimilarity(inputCase, data);
+      const mostSimilarCase = findMostSimilarCase(similarities);
+      console.log(
+        `---\n${chalk.greenBright(`Suitable Punishment Found: ${mostSimilarCase.punishment}`)}`,
+      );
+      rl.close();
+    });
+  });
+})();
